7400c585ec46b1497ba7280a5f6f141ad16041a0
prathijna <palashchiplunkar51@gmail.com>
2023-03-01 20:40:28 +0530

    Added Select Language Screen

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./views/LoginPage";
 import HomePage from "./views/HomePage";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SelectLanguage from "./views/SelectLanguage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/" element={<SelectLanguage/>}/>
         <Route path="/home" element={<HomePage/>}/>
       </Routes>
     </BrowserRouter>
---
+++ src/views/SelectLanguage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/SelectLanguage.css'
+import {FaAngleLeft} from 'react-icons/fa'
+export default function SelectLanguage() {
+  return (
+    <>
+    <div className='HomeHeader'>
+        <FaAngleLeft style={{color:'white',height:'25px',marginLeft:'20px'}}/>
+        <p className='HeaderLabel'>Language Preference</p>
+    </div>
+    <div>
+        
+    </div>
+
+    </>
+  )
+}
