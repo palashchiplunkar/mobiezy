a8e223e97ad5295a129863d9a091784d18d61b2b
prathijna <palashchiplunkar51@gmail.com>
2023-03-02 07:15:09 +0530

    Completed Select Language Screen

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -3,59 +3,53 @@ import { useNavigate } from "react-router-dom";
 import "../css/LoginStyles.css";
 
 export default function LoginPage() {
-    const navigate = useNavigate();
-    const handleSubmit = () => {
-        navigate("/home");
-    };
+  const navigate = useNavigate();
+  const handleSubmit = () => {
+    navigate("/home");
+  };
 
-    return (
-        <div className="App">
-            <img className="bg-img" src={require("../assets/BG.JPG")} />
+  return (
+    <div className="App">
+      <img className="bg-img" src={require("../assets/BG.JPG")} />
 
-            <div className="authForm">
-                <img
-                    className="header-img1"
-                    src={require("../assets/d1.png")}
-                />
-                <img
-                    className="mobicable-logo"
-                    src={require("../assets/MobiCable.jpg")}
-                />
+      <div className="authForm">
+        <img className="header-img1" src={require("../assets/d1.png")} />
+        <img
+          className="mobicable-logo"
+          src={require("../assets/MobiCable.jpg")}
+        />
 
-                <form className="login">
-                    <label className="user-label">User Name</label>
-                    <input
-                        required="true"
-                        type="text"
-                        autocomplete="off"
-                        className="username"
-                        placeholder="Enter User Name"
-                    />
+        <form className="login">
+          <label className="user-label">User Name</label>
+          <input
+            required="true"
+            type="text"
+            autocomplete="off"
+            className="username"
+            placeholder="Enter User Name"
+          />
 
-                    <label className="passwd-label">Password</label>
-                    <input
-                        required="true"
-                        type="password"
-                        autocomplete="off"
-                        className="passwd"
-                        placeholder="Enter Password"
-                    />
+          <label className="passwd-label">Password</label>
+          <input
+            required="true"
+            type="password"
+            autocomplete="off"
+            className="passwd"
+            placeholder="Enter Password"
+          />
 
-                    <div className="remember-me-div">
-                        <input className="rmcb" type="checkbox" />
-                        <label class="rememberme">Remeber me</label>
-                    </div>
+          <div className="remember-me-div">
+            <input className="rmcb" type="checkbox" />
+            <label class="rememberme">Remeber me</label>
+          </div>
 
-                    <div className="login-btn-div">
-                        <button
-                            className="loginBtn"
-                            onClick={() => handleSubmit()}
-                        >
-                            Login
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+          <div className="login-btn-div">
+            <button className="loginBtn" onClick={() => handleSubmit()}>
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/views/SelectLanguage.js
+++ src/views/SelectLanguage.js
@@ -1,17 +1,115 @@
-import React from 'react'
-import '../css/SelectLanguage.css'
-import {FaAngleLeft} from 'react-icons/fa'
+import React from "react";
+import "../css/SelectLanguage.css";
+import { FaAngleLeft } from "react-icons/fa";
+import { AiFillInfoCircle } from "react-icons/ai";
+import {BiMenu} from 'react-icons/bi'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {FaBell} from 'react-icons/fa'
+import {MdSignalCellularAlt} from 'react-icons/md'
+import {AiOutlineWifi} from 'react-icons/ai'
+import {BsBatteryFull} from 'react-icons/bs'
 export default function SelectLanguage() {
   return (
-    <>
-    <div className='HomeHeader'>
-        <FaAngleLeft style={{color:'white',height:'25px',marginLeft:'20px'}}/>
-        <p className='HeaderLabel'>Language Preference</p>
-    </div>
     <div>
-        
-    </div>
+    <img className="bg-img" src={require("../assets/BG.JPG")} />
+      <div className="HomeHeader">
+        <FaAngleLeft
+          style={{ color: "white", height: "25px", marginLeft: "20px" }}
+        />
+        <p className="HeaderLabel">Language Preference</p>
+      </div>
+      <div>
+        <p className="PreviewLabel">Preview</p>
+        <div className="PreviewBackground">
+          <div className="StatusBar">
+            <p className="Time">9:41</p>
+        <MdSignalCellularAlt  style={{position:'relative',left:'78%'}}/>
+        <AiOutlineWifi style={{position:'relative',left:'80%'}}/>
+<BsBatteryFull style={{position:'relative',left:'82%'}}/>
+          </div>
+          <div className="SearchBar">
+            <BiMenu style={{marginLeft:'5%'}}/>
+            <AiOutlineSearch style={{position:'relative',left:'70%'}}/>
+            <FaBell style={{position:'relative',left:'75%'}}/>
+          </div>
+          <div className="GreetingBox">
+            <p className="GreetingsLabel">Greetings!</p>
+            <p className="GreetingsPara">Welcome to the family of 5000+ Cable & Internet Operators</p>
+          </div>
+        </div>
+        <p className="SelectLangLabel">Select Your Language of Preference</p>
+        <div className="LanguageGroup">
+          <div className="LanguageColumn">
+            <div>
+              <input
+                type={"radio"}
+                name={"English"}
+                value={"English"}
+                className="RadioButtons"
+              />
+              <label className="RadioLabel">English</label>
+            </div>
 
-    </>
-  )
+            <div>
+              <input
+                type={"radio"}
+                name={"ಕನ್ನಡ"}
+                value={"ಕನ್ನಡ"}
+                className="RadioButtons"
+              />
+              <label className="RadioLabel">ಕನ್ನಡ</label>
+            </div>
+            <div>
+              <input
+                type={"radio"}
+                name={"தமிழ்"}
+                value={"தமிழ்"}
+                className="RadioButtons"
+              />
+              <label className="RadioLabel">தமிழ்</label>
+            </div>
+          </div>
+          <div className="LanguageColumn">
+            <div>
+              <input
+                type={"radio"}
+                name={"हिन्दी"}
+                value={"हिन्दी"}
+                className="RadioButtons"
+              />
+              <label className="RadioLabel">हिन्दी</label>
+            </div>
+            <div>
+              <input
+                type={"radio"}
+                name={"മലയാളം"}
+                value={"മലയാളം"}
+                className="RadioButtons"
+              />
+              <label className="RadioLabel">മലയാളം</label>
+            </div>
+            <div>
+              <input
+                type={"radio"}
+                name={"తెలుగు"}
+                value={"తెలుగు"}
+                className="RadioButtons"
+              />
+              <label className="RadioLabel">తెలుగు</label>
+            </div>
+          </div>
+        </div>
+        <div className="BottomMessageContainer">
+          <AiFillInfoCircle className="MDInfo" />
+          <p className="BottomMessage">
+            Language Option can be changed back at anytime, Some critical
+            information will still continue to be in English.
+          </p>
+        </div>
+        <button className="SubmitButton">
+          <span>SUBMIT</span>
+        </button>
+      </div>
+    </div>
+  );
 }
