d9fab50773ba6d070f674dc02ac3cef0f92d70f8
Maanikya <maanikya.acharya@gmail.com>
2023-03-03 18:00:44 +0530

    Navbar Updates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,70 +1,46 @@
-import "../css/BottomNav.css";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import { useState } from "react";
 import { AiFillHome } from "react-icons/ai";
 import { BiUserCircle } from "react-icons/bi";
 import { FiMoreHorizontal } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+
+import "../css/BottomNav.css";
 
 const Navbar = () => {
-  const [value, setvalue] = useState(0);
-  // const bottomNavItems = [
-  //   {
-  //     title: 'Home',
+    const [value, setvalue] = useState(0);
 
-  //     icon: <AiFillHome style={{color:'#0090DA'}}/>,
+    const navigate = useNavigate();
+    const customerDrawer = () => {
+      navigate("/customer");
+    };
 
-  //     activeIcon: <AiFillHome />
-  //   },
-
-  //   {
-  //     title: 'Search',
-
-  //     icon: <BiUserCircle style={{color:'#0090DA'}}/>,
-  //     activeIcon: <BiUserCircle/>
-
-  //   },
-
-  //   {
-  //     title: 'Notifications',
-
-  //     icon: <FiMoreHorizontal style={{color:"#0090D"}}/>,
-
-  //     activeIcon:<FiMoreHorizontal/>
-  //   }
-  // ]
-  return (
-    <div>
-      {/* <div class="navitem">
-                <img src={require("../assets/home.png")} class="nav_img" />
-                <span class="home_nav">Home</span>
-            </div>
-
-            <div class="navitem">
-                <img src={require("../assets/profile.png")} class="nav_img" />
-                <span>Customers</span>
-            </div>
-
-            <div class="navitem">
-                <img src={require("../assets/three_more.png")} class="nav_img" />
-                <span class="more_nav">More</span>
-            </div> */}
-      {/* <BottomNavigation
-        items={bottomNavItems}
-        defaultSelected={0}
-      
-        onItemClick={(item) => console.log(item)}
-      /> */}
-      <BottomNavigation
-      sx={{width:'100%',position:'fixed',bottom:0}}
-        value={value}
-        onChange={(event, newValue) => setvalue(newValue)}
-      >
-        <BottomNavigationAction label="Home" icon={<AiFillHome />} showLabel={true} />
-        <BottomNavigationAction label="Customers" icon={<BiUserCircle />} showLabel={true} />
-        <BottomNavigationAction label="More" icon={<FiMoreHorizontal />} showLabel={true} />
-      </BottomNavigation>
-    </div>
-  );
+    return (
+        <div>
+            <BottomNavigation
+                sx={{ width: "100%", position: "fixed", bottom: 0 }}
+                value={value}
+                onChange={(event, newValue) => setvalue(newValue)}
+            >
+                <BottomNavigationAction
+                    label="Home"
+                    icon={<AiFillHome />}
+                    showLabel={true}
+                />
+                <BottomNavigationAction
+                    label="Customers"
+                    icon={<BiUserCircle />}
+                    showLabel={true}
+                    onClick={() => customerDrawer()}
+                />
+                <BottomNavigationAction
+                    label="More"
+                    icon={<FiMoreHorizontal />}
+                    showLabel={true}
+                />
+            </BottomNavigation>
+        </div>
+    );
 };
 
 export default Navbar;
