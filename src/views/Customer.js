c9aec70e3dab17f50c96df462e0a31bbb7d5cdeb
prathijna <palashchiplunkar51@gmail.com>
2023-03-03 08:58:16 +0530

    Customer Page Dropdown

--- src/views/Customer.js
+++ src/views/Customer.js
@@ -1,28 +1,34 @@
-import { Dropdown } from "bootstrap";
 import React from "react";
-import Select from "react-select";
-
+import {RiSortDesc} from 'react-icons/ri'
 import "../css/Customer.css";
 
 const actions = [
-    { label: "Bangalore", value: "bangalore" },
-    { label: "Mangalore", value: "mangalore" },
-    { label: "Udupi", value: "udupi" }
+  { label: "Bangalore", value: "bangalore" },
+  { label: "Mangalore", value: "mangalore" },
+  { label: "Udupi", value: "udupi" },
 ];
 
 export default function Customer() {
-    return (
-        <div className="container">
-            <img className="home-bg-img" src={require("../assets/BG.JPG")} />
-
-            <div>
-                <div className="header-blue">
-                    <div className="area-div">
-                        <p className="area-p">Area</p>
-                        <Select className="area-dropdown" options={actions} placeholder="All Areas" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <img className="home-bg-img" src={require("../assets/BG.JPG")} />
+      <div>
+        <div className="header-blue">
+          <div className="area-div">
+            <p className="area-p">Area</p>
+            <select name="test" id="test" className="area-dropdown" placeholder="All Areas">
+              
+              <option value="blr">Bangalore</option>
+            </select>
+          </div>
+          <div className="filter-div">
+          <p className="filter-p">Filter</p>
+          <div className="filter-button">
+            <RiSortDesc style={{color:'white',margin:'auto',width:'25px',height:'50px'}}/>
+          </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
