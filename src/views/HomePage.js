ff788e52cb8d538506f667d9e06c486752f9b6fd
Maanikya <maanikya.acharya@gmail.com>
2023-03-03 18:41:03 +0530

    Correction

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -7,6 +7,7 @@ import MobileNavigation from "../components/hamnavigation";
 import HamDrawer from "../components/hamburger";
 import HamburgerDrawer from "react-hamburger-drawer";
 import { useTranslation } from "react-i18next";
+import { ClickAwayListener } from "@mui/material";
 
 export default function HomePage() {
     const { t } = useTranslation();
