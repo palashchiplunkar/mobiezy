a9cc0d094c2f175253385c41462688823feb89d3
prathijna <palashchiplunkar51@gmail.com>
2023-03-08 10:39:13 +0530

    Fixed Error

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -4,45 +4,26 @@ import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import loginAPI from "../services/authApi";
 import "../css/LoginStyles.css";
-<<<<<<< HEAD
 
-=======
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
->>>>>>> e61cac11d1835c6ba5b44e360790f8bc7d680216
 export default function LoginPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [user, setUser] = useState("");
-    const [pwd, setPwd] = useState("");
-    const [rememberMe, setRememberMe] = useState(false);
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
 
-    useEffect(() => {
-        const user = localStorage.getItem("user");
+  useEffect(() => {
+    const user = localStorage.getItem("user");
 
-        if (user) {
-            navigate("/home");
-        }
+    if (user) {
+      navigate("/home");
+    }
+  }, []);
 
-    }, []);
-
-    const handleSubmit = (e) => {
-<<<<<<< HEAD
-        loginAPI
-            .post("cableguy2-mobile-user-login-new", {
-                freshInstall: "N",
-                appVersion: "2.0.63",
-                device_id: "2714",
-                username: user,
-                password: pwd,
-            })
-
-            .then((response) => {
-                console.log(response);
-                if (response.data.messageText === "UNAUTHORIZED") {
-                    navigate("/");
-                } else {
-=======
+  const handleSubmit = (e) => {
         if(user === "" || pwd === "") {
             toast.error("Please enter username and password");
             return 
@@ -69,81 +50,79 @@ export default function LoginPage() {
                 }
                 // use async
                 navigate('/home');
->>>>>>> e61cac11d1835c6ba5b44e360790f8bc7d680216
+            if (rememberMe) {
+              localStorage.setItem("user", user);
+            }
 
-                    if (rememberMe) {
-                        localStorage.setItem("user", user);
-                    }
+            navigate("/home");
+          }
+        })
 
-                    navigate("/home");
-                }
-            })
+        .catch((e) => {
+          console.log(e);
+          alert(e.message);
+        });
+ 
+  };
 
-            .catch((e) => {
-                console.log(e);
-                alert(e.message);
-            });
-    };
-
-    const { t } = useTranslation();
-    return (
-        <div className="App">
-            <img className="bg-img" src={require("../assets/BG.JPG")} />
-            <div className="authForm">
+  const { t } = useTranslation();
+  return (
+    <div className="App">
+      <img className="bg-img" src={require("../assets/BG.JPG")} />
+      <div className="authForm">
                 <img
                     className="header-img1"
                     src={require("../assets/d1.png")}
                 />
-                <img
-                    className="mobicable-logo"
-                    src={require("../assets/MobiCable.jpg")}
-                />
+        <img
+          className="mobicable-logo"
+          src={require("../assets/MobiCable.jpg")}
+        />
 
-                {/* <form className="login"> */}
-                <label className="user-label">{t("LP_lbl_UserName")}</label>
-                <input
-                    required="true"
-                    type="text"
-                    autocomplete="off"
-                    className="username"
-                    placeholder={t("LP_data_UserName")}
-                    onChange={(e) => setUser(e.target.value)}
-                    value={user}
-                />
+        {/* <form className="login"> */}
+        <label className="user-label">{t("LP_lbl_UserName")}</label>
+        <input
+          required="true"
+          type="text"
+          autocomplete="off"
+          className="username"
+          placeholder={t("LP_data_UserName")}
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+        />
 
-                <label className="passwd-label">{t("LP_lbl_Password")}</label>
-                <input
-                    required="true"
-                    type="password"
-                    autocomplete="off"
-                    className="passwd"
-                    placeholder={t("LP_data_Password")}
-                    onChange={(e) => setPwd(e.target.value)}
-                    value={pwd}
-                />
+        <label className="passwd-label">{t("LP_lbl_Password")}</label>
+        <input
+          required="true"
+          type="password"
+          autocomplete="off"
+          className="passwd"
+          placeholder={t("LP_data_Password")}
+          onChange={(e) => setPwd(e.target.value)}
+          value={pwd}
+        />
 
-                <div className="remember-me-div">
-                    <input
-                        className="rmcb"
-                        type="checkbox"
-                        id="remberme"
-                        onChange={(e) => setRememberMe(e.target.checked)}
-                        value={rememberMe}
-                    />
-                    <label class="rememberme">{t("LP_lbl_Remember_Me")}</label>
-                </div>
-
-                <div className="login-btn-div">
-                    <button className="loginBtn" onClick={() => handleSubmit()}>
-                        {t("LP_Button_Login")}
-                    </button>
-                </div>
-                {/* </form> */}
-
-                <p className="version">{t("LP_lbl_Version")}</p>
-               
-            </div>
-            <ToastContainer />  
+        <div className="remember-me-div">
+          <input
+            className="rmcb"
+            type="checkbox"
+            id="remberme"
+            onChange={(e) => setRememberMe(e.target.checked)}
+            value={rememberMe}
+          />
+          <label class="rememberme">{t("LP_lbl_Remember_Me")}</label>
         </div>
-    );
+
+        <div className="login-btn-div">
+          <button className="loginBtn" onClick={() => handleSubmit()}>
+            {t("LP_Button_Login")}
+          </button>
+        </div>
+        {/* </form> */}
+
+        <p className="version">{t("LP_lbl_Version")}</p>
+      </div>
+            <ToastContainer />  
+    </div>
+  );
 }
