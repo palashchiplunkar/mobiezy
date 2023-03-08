1b01320f561c9b8250d9e4a9928aba13c13a377a
Maanikya <maanikya.acharya@gmail.com>
2023-03-08 13:08:16 +0530

    API

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 const loginAPI = axios.create({
-
-        baseURL: "https://ld3igodwbj.execute-api.us-west-2.amazonaws.com/prod/",
-        headers: {
-            'content-type': 'application/json'
-        }
-    }
-)
+    baseURL: "https://ld3igodwbj.execute-api.us-west-2.amazonaws.com/prod/",
+    headers: {
+        "content-type": "application/json",
+    },
+});
 
 export default loginAPI;
