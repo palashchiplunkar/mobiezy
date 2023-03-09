37c5452689c8d58c41221239ab9bdfef00cd65f3
Varshithvhegde <varshithvh@gmail.com>
2023-03-09 13:12:43 +0530

    Added CustomerStatistics

--- src/views/CustomerStatistics.js
+++ src/views/CustomerStatistics.js
@@ -15,7 +15,9 @@ export default function CustomerStatistics() {
           <p className='customer-statistics-data-label'>:</p>
         </div>
         <div className='customer-statistics-data-1'>
-          <p className='customer-statistics-data-label'>100</p>
+          <p className='customer-statistics-data-label-number'
+          style={{color: "#", fontWeight: 700,fontSize:"15px",lineHeight:"16px",letterSpacing:"0.2px"}}
+          > 1544</p>
         </div>
       </div>
       <div className="line"></div>
@@ -27,7 +29,9 @@ export default function CustomerStatistics() {
           <p className='customer-statistics-data-label'>:</p>
         </div>
         <div className='customer-statistics-data-2'>
-          <p className='customer-statistics-data-label'>100</p>
+          <p className='customer-statistics-data-label-number'
+          style={{color: "#000000", fontWeight: 700,fontSize:"15px",lineHeight:"16px",letterSpacing:"0.2px"}}
+          >51</p>
         </div>
       </div>
       <div className="line"></div>
@@ -39,7 +43,9 @@ export default function CustomerStatistics() {
           <p className='customer-statistics-data-label'>:</p>
         </div>
         <div className='customer-statistics-data-3'>
-          <p className='customer-statistics-data-label'>100</p>
+          <p className='customer-statistics-data-label-number'
+          style={{color: "#000000", fontWeight: 700,fontSize:"15px",lineHeight:"16px",letterSpacing:"0.2px"}}
+          >11</p>
         </div>
       </div>
       <div className="line"></div>
@@ -51,7 +57,9 @@ export default function CustomerStatistics() {
           <p className='customer-statistics-data-label'>:</p>
         </div>
         <div className='customer-statistics-data-4'>
-          <p className='customer-statistics-data-label'>100</p>
+          <p className='customer-statistics-data-label-number'
+          style={{color: "#DC1515", fontWeight: 700,fontSize:"15px",lineHeight:"16px",letterSpacing:"0.2px"}}
+          >1606</p>
         </div>
       </div>
     </div>
