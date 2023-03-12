8d4c0fc3e0fee3a51667842fa7ba666af5f3439f
Maanikya <maanikya.acharya@gmail.com>
2023-03-13 05:19:32 +0530

    Reformat Homepage

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -8,135 +8,150 @@ import { useState, useEffect } from "react";
 
 import "../css/HomeStyles.css";
 import "../css/global.css";
+
 export default function HomePage() {
-  const { t } = useTranslation();
-  const navigate = useNavigate();
-  const [data, setData] = useState(
-    JSON.parse(localStorage.getItem("homedata")) || {}
-  );
-  let userJson;
-  const user = localStorage.getItem("user") || sessionStorage.getItem("user");
-  if (user) {
-    userJson = JSON.parse(user);
-  }
-  const getHomeData = () => {
-    if (userJson) {
-      loginAPI
-        .post("getagentsummary", {
-          agent_id: userJson.agentId,
-        })
-        .then((response) => {
-          if (response.data.report[0]) {
-            setData(response.data.report[0]);
-          }
-          localStorage.setItem(
-            "homedata",
-            JSON.stringify(response.data.report[0])
-          );
-          sessionStorage.setItem(
-            "homedata",
-            JSON.stringify(response.data.report[0])
-          );
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+    const [data, setData] = useState(
+        JSON.parse(localStorage.getItem("homedata")) || {}
+    );
+
+    let userJson;
+
+    const user = localStorage.getItem("user") || sessionStorage.getItem("user");
+
+    if (user) {
+        userJson = JSON.parse(user);
     }
-  };
-  useEffect(() => {
-    // loginAPI
-    //   .post("getagentsummary", { agent_id: "11276" })
-    //   .then((response) => {
-    //     console.log(response.data.report[0]);
-    //     if (response.data.report[0]) {
-    //       setData(response.data.report[0]);
-    //     }
 
-    //     localStorage.setItem(
-    //       "homedata",
-    //       JSON.stringify(response.data.report[0])
-    //     );
-    //   })
+    const getHomeData = () => {
+        if (userJson) {
+            loginAPI
+                .post("getagentsummary", {
+                    agent_id: userJson.agentId,
+                })
 
-    //   .catch((e) => {
-    //     console.log(e);
-    //   });
-    getHomeData();
-  }, []);
+                .then((response) => {
+                    if (response.data.report[0]) {
+                        setData(response.data.report[0]);
+                    }
 
-  return (
-    <>
-      <div className="container">
-        <img className="home-bg-img" src={require("../assets/BG.JPG")} />
-        <div>
-          <div className="headerblue">
-            <MobileNavigation />
-            <p className="Company_name">
-              {userJson ?userJson.operatorName:null}
-            </p>
+                    localStorage.setItem(
+                        "homedata",
+                        JSON.stringify(response.data.report[0])
+                    );
 
-            <div className="profile-img-div">
-              <img
-                src={require("../assets/profile.jpg")}
-                className="profile_img"
-              />
+                    sessionStorage.setItem(
+                        "homedata",
+                        JSON.stringify(response.data.report[0])
+                    );
+                })
+                
+                .catch((e) => {
+                    console.log(e);
+                });
+        }
+    };
+
+    useEffect(() => {
+        // loginAPI
+        //   .post("getagentsummary", { agent_id: "11276" })
+        //   .then((response) => {
+        //     console.log(response.data.report[0]);
+        //     if (response.data.report[0]) {
+        //       setData(response.data.report[0]);
+        //     }
+
+        //     localStorage.setItem(
+        //       "homedata",
+        //       JSON.stringify(response.data.report[0])
+        //     );
+        //   })
+
+        //   .catch((e) => {
+        //     console.log(e);
+        //   });
+        getHomeData();
+    }, []);
+
+    return (
+        <>
+            <div className="container">
+                <img
+                    className="home-bg-img"
+                    src={require("../assets/BG.JPG")}
+                />
+                <div>
+                    <div className="headerblue">
+                        <MobileNavigation />
+                        <p className="Company_name">
+                            {userJson ? userJson.operatorName : null}
+                        </p>
+
+                        <div className="profile-img-div">
+                            <img
+                                src={require("../assets/profile.jpg")}
+                                className="profile_img"
+                            />
+                        </div>
+                    </div>
+
+                    <p className="user_name">
+                        {t("HO_lbl_wish")}{" "}
+                        {userJson ? userJson.agentName : null}
+                    </p>
+
+                    <div className="amt-due-today-div">
+                        <label className="amt-due-today-content">
+                            {t("HO_lbl_Unpaid")}
+                        </label>
+                        <label className="amt-due-today-content">
+                            {data.pending_amount}
+                        </label>
+                    </div>
+
+                    <div
+                        className="amt-collected-month-div"
+                        onClick={() => navigate("/monthlyreport")}
+                    >
+                        <label className="amt-collected-month-content">
+                            {t("HO_lbl_Collected")}
+                        </label>
+                        <label className="amt-collected-month-content">
+                            {data.collected_amount}
+                        </label>
+                    </div>
+
+                    <div
+                        className="amt-collected-today-div"
+                        onClick={() => navigate("/dailyReport")}
+                    >
+                        <label className="amt-collected-today-content">
+                            {t("HO_lbl_Daily")}
+                        </label>
+                        <label className="amt-collected-today-content">
+                            {data.daily_collection_amount}
+                        </label>
+                    </div>
+
+                    <div className="complaints-div">
+                        <label className="complaints-content">
+                            {t("HO_lbl_Complaints")}
+                        </label>
+                        <label className="complaints-content">
+                            {data.no_of_complaints}
+                        </label>
+                    </div>
+
+                    <div className="collect-btn-div">
+                        <button className="collectBtn" type="submit">
+                            {t("HO_button_Collect_Bill")}
+                        </button>
+                    </div>
+                </div>
             </div>
-          </div>
-
-          <p className="user_name">
-            {t("HO_lbl_wish")} {userJson? userJson.agentName:null}
-          </p>
-
-          <div className="amt-due-today-div">
-            <label className="amt-due-today-content">
-              {t("HO_lbl_Unpaid")}
-            </label>
-            <label className="amt-due-today-content">
-              {data.pending_amount}
-            </label>
-          </div>
-
-          <div
-            className="amt-collected-month-div"
-            onClick={() => navigate("/monthlyreport")}
-          >
-            <label className="amt-collected-month-content">
-              {t("HO_lbl_Collected")}
-            </label>
-            <label className="amt-collected-month-content">
-              {data.collected_amount}
-            </label>
-          </div>
-
-          <div
-            className="amt-collected-today-div"
-            onClick={() => navigate("/dailyReport")}
-          >
-            <label className="amt-collected-today-content">
-              {t("HO_lbl_Daily")}
-            </label>
-            <label className="amt-collected-today-content">
-              {data.daily_collection_amount}
-            </label>
-          </div>
-
-          <div className="complaints-div">
-            <label className="complaints-content">
-              {t("HO_lbl_Complaints")}
-            </label>
-            <label className="complaints-content">
-              {data.no_of_complaints}
-            </label>
-          </div>
-
-          <div className="collect-btn-div">
-            <button className="collectBtn" type="submit">
-              {t("HO_button_Collect_Bill")}
-            </button>
-          </div>
-        </div>
-      </div>
-      <Navbar value={0} />
-    </>
-  );
+            <Navbar value={0} />
+        </>
+    );
 }
