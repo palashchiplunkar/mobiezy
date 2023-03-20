94fb7d9d7f5acdf4fd43b446ddc713908c487463
Varshithvhegde <varshithvh@gmail.com>
2023-03-20 21:58:52 +0530

    Nothing more testsing

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -74,6 +74,9 @@ export default function HomePage() {
   useEffect(() => {
     // get back click event and also create a alert event
     window.history.pushState({}, '');
+    window.addEventListener('load', function() {
+        window.history.pushState({}, '')
+      })
     window.addEventListener("popstate", function (e) {
       e.preventDefault();
       e.stopPropagation();
