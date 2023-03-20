efcdd8caadbfb4c6146fe708ca9c192ea6eb47b1
prathijna <palashchiplunkar51@gmail.com>
2023-03-20 17:29:47 +0530

    Fixed issues in login screen

--- src/views/LoginPage.js
+++ src/views/LoginPage.js
@@ -11,187 +11,193 @@ import "../css/global.css";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function LoginPage() {
-    const navigate = useNavigate();
-    const [isLoading, setIsLoading] = useState(false);
-    const [Error, setError] = useState("");
-    const [user, setUser] = useState("");
-    const [pwd, setPwd] = useState("");
-    const [rememberMe, setRememberMe] = useState(false);
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [Error, setError] = useState("");
+  const [UError, setUError] = useState("");
+  const [PError, setPError] = useState("");
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
 
-    useEffect(() => {
-        // const user1 =sessionStorage.setItem("user","test");
-        // console.log(user1)
+  useEffect(() => {
+    // const user1 =sessionStorage.setItem("user","test");
+    // console.log(user1)
 
-        const user = localStorage.getItem("user");
+    const user = localStorage.getItem("user");
 
-        if (user) {
-            navigate("/home");
-        }
-    }, []);
+    if (user) {
+      navigate("/home");
+    }
+  }, []);
 
-    const onchangeuser = (value) => {
-        setUser(value);
-        setError("");
-    };
+  const onchangeuser = (value) => {
+    setUser(value);
+    setError("");
+    setUError("");
+  };
 
-    const onchangepass = (value) => {
-        setPwd(value);
-        setError("");
-    };
+  const onchangepass = (value) => {
+    setPwd(value);
+    setError("");
+    setPError("");
+  };
 
-    const handleSubmit = (e) => {
-        setIsLoading(true);
-        if (user === "" && pwd === "") {
-            setError("Please enter Username or Password");
-            setIsLoading(false);
-            return;
-        }
+  const handleSubmit = (e) => {
+    setIsLoading(true);
+    if (user === "" && pwd === "") {
+      setUError("Please enter Username");
+      setPError("Please enter Password");
+      setIsLoading(false);
+      return;
+    } else if (user === "") {
+      setUError("Please Enter Username");
+      setIsLoading(false);
+      return;
+    } else if (pwd === "") {
+      setPError("Please Enter Password");
+      setIsLoading(false);
+      return;
+    }
 
-        else if (user === "") {
-            setError("Please enter Username");
-            setIsLoading(false);
-            return;
-        }
+    loginAPI
+      .post("cableguy2-mobile-user-login-new", {
+        freshInstall: "N",
+        appVersion: "2.0.63",
+        device_id: "2714",
+        username: user,
+        password: pwd,
+      })
 
-        else if (pwd === "") {
-            setError("Please enter Password");
-            setIsLoading(false);
-            return;
-        }
-
-        loginAPI
-            .post("cableguy2-mobile-user-login-new", {
-                freshInstall: "N",
-                appVersion: "2.0.63",
-                device_id: "2714",
+      .then((response) => {
+        console.log(response);
+        if (response.data.messageText === "UNAUTHORIZED") {
+          setUError("Invalid Username!");
+          setPError("Invalid Password!");
+          navigate("/");
+        } else {
+          if (rememberMe) {
+            localStorage.setItem(
+              "user",
+              JSON.stringify({
                 username: user,
-                password: pwd,
-            })
+                agentId: response.data.agentId,
+                agentName: response.data.agentName,
+                operatorName: response.data.operatorName,
+              })
+            );
+          } else {
+            sessionStorage.setItem(
+              "user",
+              JSON.stringify({
+                username: user,
+                agentId: response.data.agentId,
+                agentName: response.data.agentName,
+                operatorName: response.data.operatorName,
+              })
+            );
+          }
+          navigate("/home");
+        }
+        setIsLoading(false);
+      })
 
-            .then((response) => {
-                console.log(response);
-                if (response.data.messageText === "UNAUTHORIZED") {
-                    // toast.error("Invalid username or password");
-                    setError("Invalid Username or Password");
-                    navigate("/");
-                } else {
-                    if (rememberMe) {
-                        localStorage.setItem(
-                            "user",
-                            JSON.stringify({
-                                username: user,
-                                agentId: response.data.agentId,
-                                agentName: response.data.agentName,
-                                operatorName: response.data.operatorName,
-                            })
-                        );
-                    } else {
-                        sessionStorage.setItem(
-                            "user",
-                            JSON.stringify({
-                                username: user,
-                                agentId: response.data.agentId,
-                                agentName: response.data.agentName,
-                                operatorName: response.data.operatorName,
-                            })
-                        );
-                    }
-                    navigate("/home");
-                }
-                setIsLoading(false);
-            })
+      .catch((e) => {
+        console.log(e);
+        setError(e.message);
+        setIsLoading(false);
+      });
+  };
 
-            .catch((e) => {
-                console.log(e);
-                setError(e.message);
-                setIsLoading(false);
-            });
-    };
+  const { t } = useTranslation();
+  return (
+    <div className="App">
+      <div className="ImageContainer">
+        {/* <img className="bg-img" src={require("../assets/BG.JPG")} /> */}
+        <img className="header-img1" src={require("../assets/d1.png")} />
+        <img
+          className="mobicable-logo"
+          src={require("../assets/MobiCable.jpg")}
+        />
+      </div>
+      <div className="authForm">
+        {/* <form className="login"> */}
+        <div className="inputGroup">
+          <label className="user-label">{t("LP_lbl_UserName")}</label>
+          <input
+            required={true}
+            type="text"
+            autoComplete="off"
+            className="username"
+            placeholder={t("LP_data_UserName")}
+            onChange={(e) => onchangeuser(e.target.value)}
+            value={user}
+            style={{ borderBottomColor: UError ? "red" : "#333333" }}
+          />
+            <p
+              style={{
+                color: "red",
+                visibility: UError? "visible" : "hidden",
 
-    const { t } = useTranslation();
-    return (
-        <div className="App">
-            <img className="bg-img" src={require("../assets/BG.JPG")} />
-            <div className="authForm">
-                <img
-                    className="header-img1"
-                    src={require("../assets/d1.png")}
-                />
-                <img
-                    className="mobicable-logo"
-                    src={require("../assets/MobiCable.jpg")}
-                />
-
-                {/* <form className="login"> */}
-                <label className="user-label">{t("LP_lbl_UserName")}</label>
-                <input
-                    required={true}
-                    type="text"
-                    autoComplete="off"
-                    className="username"
-                    placeholder={t("LP_data_UserName")}
-                    onChange={(e) => onchangeuser(e.target.value)}
-                    value={user}
-                    style={{ borderBottomColor: Error ? "red" : "#333333" }}
-                />
-
-                <label className="passwd-label">{t("LP_lbl_Password")}</label>
-                <input
-                    required={true}
-                    type="password"
-                    autoComplete="off"
-                    className="passwd"
-                    placeholder={t("LP_data_Password")}
-                    onChange={(e) => onchangepass(e.target.value)}
-                    value={pwd}
-                    style={{ borderBottomColor: Error ? "red" : "#333333" }}
-                />
-                {Error && (
-                    <p
-                        style={{
-                            color: "red",
-                            textAlign: "center",
-                            position: "relative",
-                            marginTop: "5%",
-                        }}
-                    >
-                        {Error}
-                    </p>
-                )}
-                <div className="remember-me-div">
-                    <input
-                        className="rmcb"
-                        type="checkbox"
-                        id="remberme"
-                        onChange={(e) => setRememberMe(e.target.checked)}
-                        value={rememberMe}
-                    />
-                    <label className="rememberme">
-                        {t("LP_lbl_Remember_Me")}
-                    </label>
-                </div>
-
-                <div className="login-btn-div">
-                    <button className="loginBtn" onClick={() => handleSubmit()}>
-                        {t("LP_Button_Login")}
-                    </button>
-                </div>
-
-                {/* </form> */}
-                <p className="version">{t("LP_lbl_Version")}</p>
-
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                    {isLoading && (
-                        <ReactLoading
-                            type={"bars"}
-                            color={"#0090da"}
-                            height={75}
-                            width={75}
-                        />
-                    )}
-                </div>
-            </div>
-            <ToastContainer />
+              }}
+            >
+              {UError}
+            </p>
+    
         </div>
-    );
+        <div className="inputGroup">
+          <label className="passwd-label">{t("LP_lbl_Password")}</label>
+          <input
+            required={true}
+            type="password"
+            autoComplete="off"
+            className="passwd"
+            placeholder={t("LP_data_Password")}
+            onChange={(e) => onchangepass(e.target.value)}
+            value={pwd}
+            style={{ borderBottomColor: PError ? "red" : "#333333" }}
+          />
+ 
+            <p
+              style={{
+                color: "red",
+                visibility: PError? "visible" : "hidden",
+              }}
+            >
+              {PError}
+            </p>
+        </div>
+        <div className="remember-me-div">
+          <input
+            className="rmcb"
+            type="checkbox"
+            id="remberme"
+            onChange={(e) => setRememberMe(e.target.checked)}
+            value={rememberMe}
+          />
+          <label className="rememberme">{t("LP_lbl_Remember_Me")}</label>
+        </div>
+
+       
+      </div>
+      <div className="login-btn-div">
+          <button className="loginBtn" onClick={() => handleSubmit()}>
+            {t("LP_Button_Login")}
+          </button>
+        </div>
+      {/* </form> */}
+      <p className="version">{t("LP_lbl_Version")}</p>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        {isLoading && (
+          <ReactLoading
+            type={"bars"}
+            color={"#0090da"}
+            height={75}
+            width={75}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
