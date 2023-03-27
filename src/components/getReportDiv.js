8c658738abdc9b579af198e77523d96eeba2f7c4
Maanikya <maanikya.acharya@gmail.com>
2023-03-27 11:36:45 +0530

    Error Correction

--- src/components/getReportDiv.js
+++ src/components/getReportDiv.js
@@ -24,57 +24,59 @@ const GetReportDiv = (props) => {
         return OwnerSelectionData;
     };
 
-  const handleSelection = () => {
-    setOwnerDataforDropdown([]);
-    setIsLoading(true);
-    const selectedOwner = document.getElementById("ownerselect").value;
-    API.dailyReportAPI({
-      agentId: selectedOwner,
-      operatorId: "1603",
-      dailyReport: "Y",
-      considerAgentType: "N",
-    })
-      .then((response) => {
-        setIsLoading(false);
-        console.log(response.data.report);
-        setOwnerDataforDropdown(response.data.report);
-        if (response.data.report.length > 0) {
-          setLength(response.data.report.length);
-          setCollectedAmount(response.data.report[0].totalCollectedAmount);
-        } else {
-          console.log("No data found");
-          setLength(0);
-          setCollectedAmount(0);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+    const handleSelection = () => {
+        setOwnerDataforDropdown([]);
+        setIsLoading(true);
+        const selectedOwner = document.getElementById("ownerselect").value;
+        API.dailyReportAPI({
+            agentId: selectedOwner,
+            operatorId: "1603",
+            dailyReport: "Y",
+            considerAgentType: "N",
+        })
+            .then((response) => {
+                setIsLoading(false);
+                console.log(response.data.report);
+                setOwnerDataforDropdown(response.data.report);
+                if (response.data.report.length > 0) {
+                    setLength(response.data.report.length);
+                    setCollectedAmount(
+                        response.data.report[0].totalCollectedAmount
+                    );
+                } else {
+                    console.log("No data found");
+                    setLength(0);
+                    setCollectedAmount(0);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     const AllData = () => {
         setAgentData("owner");
     };
 
-  return (
-    <div className="get-report-div">
-      <select
-        className="get-report-dropdown"
-        id="ownerselect"
-        onChange={() => setSelected(selected)}
-      >
-        <option value="11276" onClick={AllData}>
-          Owner Sur ...
-        </option>
-        <option value="0">Office</option>
-        <option value="1">Online</option>
-        <OwnerSelection />
-      </select>
-      <button className="get-report-btn" onClick={handleSelection}>
-        Get Report
-      </button>
-    </div>
-  );
+    return (
+        <div className="get-report-div">
+            <select
+                className="get-report-dropdown"
+                id="ownerselect"
+                onChange={() => setSelected(selected)}
+            >
+                <option value="11276" onClick={AllData}>
+                    Owner Sur ...
+                </option>
+                <option value="0">Office</option>
+                <option value="1">Online</option>
+                <OwnerSelection />
+            </select>
+            <button className="get-report-btn" onClick={handleSelection}>
+                Get Report
+            </button>
+        </div>
+    );
 };
 
 export default GetReportDiv;
