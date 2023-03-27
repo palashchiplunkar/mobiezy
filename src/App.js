1d6bc9143449e9957b8f98e4f7cbcc6c56baa0d5
Maanikya <maanikya.acharya@gmail.com>
2023-03-27 19:39:37 +0530

    Packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  redirect,
-  Navigate,
+    BrowserRouter,
+    Routes,
+    Route,
 } from "react-router-dom";
 
 import LoginPage from "./views/LoginPage";
@@ -23,31 +21,43 @@ import Complaints from "./views/Complaints";
 import Print from "./views/print";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<PrivateRoutes/>}>
-          <Route path="/select" element={<SelectLanguage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/customer" element={<Customer />} />
-          <Route path="/customerDrawer" element={<customerDrawer />} />
-          <Route path="/monthlyReport" element={<MonthReport />} />
-          <Route path="/dailyReport" element={<DailyReport />} />
-          <Route path="/more" element={<MoreOptions />} />
-          <Route path="/areaWiseReport" element={<AreaWiseReport />} />
-          <Route
-            path="/subExpiryReport"
-            element={<SubscriptionExpiryReport />}
-          />
-          <Route path="/collectPayment" element={<CollectPayment />} />
-          <Route path="/customerStatistics" element={<CustomerStatistics/>}/>
-          <Route path="/complaints" element={<Complaints/>}/>
-        </Route>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/print" element={<Print />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<PrivateRoutes />}>
+                    <Route path="/select" element={<SelectLanguage />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/customer" element={<Customer />} />
+                    <Route
+                        path="/customerDrawer"
+                        element={<customerDrawer />}
+                    />
+                    <Route path="/monthlyReport" element={<MonthReport />} />
+                    <Route path="/dailyReport" element={<DailyReport />} />
+                    <Route path="/more" element={<MoreOptions />} />
+                    <Route
+                        path="/areaWiseReport"
+                        element={<AreaWiseReport />}
+                    />
+                    <Route
+                        path="/subExpiryReport"
+                        element={<SubscriptionExpiryReport />}
+                    />
+                    <Route
+                        path="/collectPayment"
+                        element={<CollectPayment />}
+                    />
+                    <Route
+                        path="/customerStatistics"
+                        element={<CustomerStatistics />}
+                    />
+                    <Route path="/complaints" element={<Complaints />} />
+                </Route>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/print" element={<Print />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/views/print.js
+++ src/views/print.js
@@ -1,11 +1,15 @@
 import React from "react";
+import escpos from "escpos";
 
 import "../css/print.css";
 
 export default function Print() {
 
-    const search = () => {
-        navigator.bluetooth
+    const bluetoothDevice = new escpos.Bluetooth("02:1D:A4:91:66:CC", 1);
+    const bluetoothPrinter = new escpos.Printer(bluetoothDevice);
+
+    const search = async () => {
+        await navigator.bluetooth
             .requestDevice({
                 acceptAllDevices: true,
             })
@@ -13,10 +17,13 @@ export default function Print() {
             .then((device) => {
                 // Human-readable name of the device.
                 console.log(device.name);
-                // console.log(s);
 
                 // Attempts to connect to remote GATT Server.
-                return device.gatt.connect();
+                const server = device.gatt.connect();
+
+                console.log(server);
+                window.print();
+                // console.log(service);
             })
 
             .catch((error) => {
