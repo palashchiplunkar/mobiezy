dc7161225629443bcf5eda34134164f2665b5464
Maanikya <maanikya.acharya@gmail.com>
2023-03-27 11:51:38 +0530

    Removed Unsed Function

--- src/components/getReportDiv.js
+++ src/components/getReportDiv.js
@@ -13,11 +13,11 @@ const GetReportDiv = (props) => {
         setIsLoading,
         setCollectedAmount,
         setLength,
-        operatorId
+        operatorId,
     } = props;
 
     const [selected, setSelected] = useState(null);
-    console.log(operatorId)
+    console.log(operatorId);
     const OwnerSelection = () => {
         const OwnerSelectionData = agentData.map((data) => {
             return <option value={data.Agent_Id}>{data.Name}</option>;
@@ -25,36 +25,34 @@ const GetReportDiv = (props) => {
         return OwnerSelectionData;
     };
 
-  const handleSelection = () => {
-    setOwnerDataforDropdown([]);
-    setIsLoading(true);
-    const selectedOwner = document.getElementById("ownerselect").value;
-    API.dailyReportAPI({
-      agentId: selectedOwner,
-      operatorId: operatorId,
-      dailyReport: "Y",
-      considerAgentType: "N",
-    })
-      .then((response) => {
-        setIsLoading(false);
-        console.log(response.data.report);
-        setOwnerDataforDropdown(response.data.report);
-        if (response.data.report.length > 0) {
-          setLength(response.data.report.length);
-          setCollectedAmount(response.data.report[0].totalCollectedAmount);
-        } else {
-          console.log("No data found");
-          setLength(0);
-          setCollectedAmount(0);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-    const AllData = () => {
-        setAgentData("owner");
+    const handleSelection = () => {
+        setOwnerDataforDropdown([]);
+        setIsLoading(true);
+        const selectedOwner = document.getElementById("ownerselect").value;
+        API.dailyReportAPI({
+            agentId: selectedOwner,
+            operatorId: operatorId,
+            dailyReport: "Y",
+            considerAgentType: "N",
+        })
+            .then((response) => {
+                setIsLoading(false);
+                console.log(response.data.report);
+                setOwnerDataforDropdown(response.data.report);
+                if (response.data.report.length > 0) {
+                    setLength(response.data.report.length);
+                    setCollectedAmount(
+                        response.data.report[0].totalCollectedAmount
+                    );
+                } else {
+                    console.log("No data found");
+                    setLength(0);
+                    setCollectedAmount(0);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
 
     return (
@@ -64,8 +62,8 @@ const GetReportDiv = (props) => {
                 id="ownerselect"
                 onChange={() => setSelected(selected)}
             >
-                <option value="11276" onClick={AllData}>
-                    Owner Sur ...
+                <option value={agentData.agentId}>
+                    Owner Sur...
                 </option>
                 <option value="0">Office</option>
                 <option value="1">Online</option>
