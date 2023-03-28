f41ccbe4b940c561f0b13824184f8d434edc6cd4
Maanikya <maanikya.acharya@gmail.com>
2023-03-28 18:02:48 +0530

    First Bluetooth Print Implementation

--- src/views/print.js
+++ src/views/print.js
@@ -3,56 +3,51 @@ import React from "react";
 import "../css/print.css";
 
 export default function Print() {
+    async function requestSerialPort() {
+        try {
+            const port = await navigator.serial.requestPort();
+            console.log("Serial port selected:", port);
+            return port;
+        } catch (error) {
+            console.error("Failed to request serial port:", error);
+        }
+    }
 
-    // async function requestSerialPort() {
-    //     try {
-    //         const port = await navigator.serial.requestPort();
-    //         console.log("Serial port selected:", port);
-    //         return port;
-    
-    //     } catch (error) {
-    //         console.error("Failed to request serial port:", error);
-    //     }
-    // }
+    async function connectToBluetoothDevice(port) {
+        try {
+            await port.open({
+                baudRate: 115200,
+                dataBits: 8,
+                stopBits: 1,
+                parity: "none",
+            });
+            console.log("Connected to serial port:", port);
 
-    // async function connectToBluetoothDevice(port) {
-    //     try {
-    //         await port.open({
-    //             baudRate: 115200,
-    //             dataBits: 8,
-    //             stopBits: 1,
-    //             parity: "none",
-    //         });
-    //         console.log("Connected to serial port:", port);
-    //         const encoder = new TextEncoder();
-    //         const writer = port.writable.getWriter();
-    //         await writer.write(encoder.encode("PING"));
-    //         writer.releaseLock();
-    //         // Send commands to the Bluetooth device
+            // Send commands to the Bluetooth device
+        } catch (error) {
+            console.error("Failed to connect to serial port:", error);
+        }
+    }
 
-    //     } catch (error) {
-    //         console.error("Failed to connect to serial port:", error);
-    //     }
-    // }
+    async function sendCommand(port, command) {
+        try {
+            const encoder = new TextEncoder();
+            const writer = port.writable.getWriter();
+            await writer.write(encoder.encode("JAI!!!!!!!!"));
 
-    // async function sendCommand(port, command) {
-    //     try {
-    //         const encoder = new TextEncoder();
-    //         await port.write(encoder.encode(command));
-    //         console.log("Command sent:", command);
-    //         writer.releaseLock();
-    //     } catch (error) {
-    //         console.error("Failed to send command:", error);
-    //     }
-    // }
+            console.log("Command sent:", command);
+        } catch (error) {
+            console.error("Failed to send command:", error);
+        }
+    }
 
-    // async function connectToDevice() {
-    //     const port = await requestSerialPort();
-    //     if (port) {
-    //         await connectToBluetoothDevice(port);
-    //         await sendCommand(port, "AT\r\n");
-    //     }
-    // }
+    async function connectToDevice() {
+        const port = await requestSerialPort();
+        if (port) {
+            await connectToBluetoothDevice(port);
+            await sendCommand(port, "AT\r\n");
+        }
+    }
 
     const search = async () => {
         // await navigator.bluetooth
@@ -68,27 +63,36 @@ export default function Print() {
 
         let device = await navigator.bluetooth.requestDevice({
             acceptAllDevices: true,
+            optionalServices: ["e7810a71-73ae-499d-8c15-faa9aef0c3f2"]
         });
 
         let server = await device.gatt.connect();
+
         console.log(server);
+        
         let service = await server.getPrimaryService(
-            "00001800-0000-1000-8000-00805f9b34fb"
+            "e7810a71-73ae-499d-8c15-faa9aef0c3f2"
         );
+        
         // let services = await server.getPrimaryServices();
+        
         console.log(service);
-        let characteristic = await service.getCharacteristic(
-            "0000182a-0000-1000-8000-00805f9b34fb"
-        );
-        // console.log(characteristic.properties.write = true);
-        // let characteristic = await service.getCharacteristic(0x1123);
+        
         // let characteristics = await service.getCharacteristics();
+        // let characteristic = await service.getCharacteristic(
+        //     "0000182a-0000-1000-8000-00805f9b34fb"
+        // );
+
+        let characteristic = await service.getCharacteristic(
+            "bef8d6c9-9c21-4c9e-b632-bd58c1009f9f"
+        );
+        
         console.log(characteristic);
         const encoder = new TextEncoder();
-        const data = encoder.encode("Hello, world!");
-        console.log(characteristic.writeValue(data));
+        const data = encoder.encode("\n\n\n\n\n\Hello, world!\na");
+        // console.log(characteristic.writeValue(data));
         // characteristic.readValue();
-        // characteristics.writeValue("Jai");
+        characteristic.writeValue(data);
     };
 
     return (
