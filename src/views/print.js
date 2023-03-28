2db22b9bacb980bb7488a38a0694cb39083fff54
Maanikya <maanikya.acharya@gmail.com>
2023-03-28 14:42:40 +0530

    Correctionss

--- src/views/print.js
+++ src/views/print.js
@@ -20,13 +20,13 @@ export default function Print() {
                 // optionalServices: [0x2A00]
             })
 
-    //         .then((device) => {
-    //             // Human-readable name of the device.
-    //             console.log(device.name);
+            .then((device) => {
+                // Human-readable name of the device.
+                console.log(device.name);
 
-    //             // Attempts to connect to remote GATT Server.
-    //             const server = device.gatt.connect();
-
+                // Attempts to connect to remote GATT Server.
+                const server = device.gatt.connect();
+            })
                
     };
 
