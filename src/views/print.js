1e475a2a0c99a4aa7131b716bf0ed8d4cbb6a334
Varshithvhegde <varshithvh@gmail.com>
2023-03-28 07:58:15 +0530

    merge

--- src/views/print.js
+++ src/views/print.js
@@ -8,28 +8,58 @@ export default function Print() {
     // const bluetoothDevice = new escpos.Bluetooth("02:1D:A4:91:66:CC", 1);
     // const bluetoothPrinter = new escpos.Printer(bluetoothDevice);
 
-    // const search = async () => {
-    //     await navigator.bluetooth
-    //         .requestDevice({
-    //             acceptAllDevices: true,
-    //         })
+    const search = async () => {
+        await navigator.bluetooth
+            .requestDevice({
+                acceptAllDevices: true,
+            })
 
-    //         .then((device) => {
-    //             // Human-readable name of the device.
-    //             console.log(device.name);
+            .then((device) => {
+                // Human-readable name of the device.
+                console.log(device.name);
 
-    //             // Attempts to connect to remote GATT Server.
-    //             const server = device.gatt.connect();
+                // Attempts to connect to remote GATT Server.
+                const server = device.gatt.connect();
 
-    //             console.log(server);
-    //             window.print();
-    //             // console.log(service);
-    //         })
+                console.log(server);
+                window.print();
+                // console.log(service);
+            })
 
-    //         .catch((error) => {
-    //             console.error(error);
-    //         });
-    // };
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    // const search = () => {
+    //     var SERVICE = '000018f0-0000-1000-8000-00805f9b34fb';
+    //     var WRITE = '00002af1-0000-1000-8000-00805f9b34fb';
+        
+    //     var DATA = ''
+    //         + '\x1B' + '\x61' + '\x31'                                              // center align
+    //         + '\x1D' + '\x21' + '\x11' + 'Hello\nBluetooth!\n\n'                    // double font size
+    //         + '\x1D' + '\x21' + '\x00' + '... from your friends\nat https://qz.io'  // normal font size
+    //         + '\n\n\n\n\n\n\n';                                                     // feed paper
+        
+    //     var deviceHandle;
+    //     navigator.bluetooth.requestDevice({ filters: [{ services: [SERVICE]}] }).then(device => {
+    //         console.log(device);
+    //         deviceHandle = device;
+    //         return device.gatt.connect()
+    //     }).then(server => {
+    //         console.log(server);
+    //         return server.getPrimaryService(SERVICE);
+    //     }).then(service => {
+    //         console.log(service);
+    //         return service.getCharacteristic(WRITE);
+    //     }).then(channel => {
+    //         console.log(channel);
+    //         return channel.writeValue(new TextEncoder("utf-8").encode(DATA));
+    //     }).catch(error => {
+    //         console.error(error)
+    //     }).finally(() => {
+    //         deviceHandle.gatt.disconnect();
+    //     });
+    //         };
 
     // return (
     //     <>
@@ -53,7 +83,8 @@ export default function Print() {
 
     // Write a code to connexcta adn print something on thermal printer 
     // bluetoothPrinter
-    
+    // From https://github.com/WebBluetoothCG/demos/blob/gh-pages/bluetooth-printer/index.html
+  
 
 
 }
