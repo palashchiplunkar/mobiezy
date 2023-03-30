90fa54d187258301ea415d298045ba83338ee462
Maanikya <maanikya.acharya@gmail.com>
2023-03-30 14:28:32 +0530

    Successfully Implemented Internet Check

--- src/App.js
+++ src/App.js
@@ -35,37 +35,50 @@ function App() {
             window.location.reload();
         }
     }
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<PrivateRoutes />}>
-          <Route path="/select" element={<SelectLanguage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/customer" element={<Customer />} />
-          <Route path="/customerDrawer" element={<customerDrawer />} />
-          <Route path="/monthlyReport" element={<MonthReport />} />
-          <Route path="/dailyReport" element={<DailyReport />} />
-          <Route path="/more" element={<MoreOptions />} />
-          <Route path="/areaWiseReport" element={<AreaWiseReport />} />
-          <Route
-            path="/subExpiryReport"
-            element={<SubscriptionExpiryReport />}
-          />
-          <Route path="/collectPayment" element={<CollectPayment />} />
-          <Route path="/customerStatistics" element={<CustomerStatistics />} />
-          <Route path="/complaints" element={<Complaints />} />
-          <Route path="/history" element={<PaymentHistory />} />
-          <Route path="/print" element={<Print />} />
-          <Route path="/historyVisit" element={<HistoryVisit />} />
-          <Route path="/recordVisit" element={<RecordVisit />} />
-          <Route path="/editCustomer" element={<EditCustomer />} />
-          <Route path="/editSetTopBox" element={<EditSetTopBox />} />
-          <Route path="/stbHistory" element={<STBHistory />} />
-        </Route>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<PrivateRoutes />}>
+                    <Route path="/select" element={<SelectLanguage />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/customer" element={<Customer />} />
+                    <Route
+                        path="/customerDrawer"
+                        element={<customerDrawer />}
+                    />
+                    <Route path="/monthlyReport" element={<MonthReport />} />
+                    <Route path="/dailyReport" element={<DailyReport />} />
+                    <Route path="/more" element={<MoreOptions />} />
+                    <Route
+                        path="/areaWiseReport"
+                        element={<AreaWiseReport />}
+                    />
+                    <Route
+                        path="/subExpiryReport"
+                        element={<SubscriptionExpiryReport />}
+                    />
+                    <Route
+                        path="/collectPayment"
+                        element={<CollectPayment />}
+                    />
+                    <Route
+                        path="/customerStatistics"
+                        element={<CustomerStatistics />}
+                    />
+                    <Route path="/complaints" element={<Complaints />} />
+                    <Route path="/history" element={<PaymentHistory />} />
+                    <Route path="/print" element={<Print />} />
+                    <Route path="/historyVisit" element={<HistoryVisit />} />
+                    <Route path="/recordVisit" element={<RecordVisit />} />
+                    <Route path="/editCustomer" element={<EditCustomer />} />
+                    <Route path="/editSetTopBox" element={<EditSetTopBox />} />
+                    <Route path="/stbHistory" element={<STBHistory />} />
+                </Route>
+                <Route path="/" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/views/Offline.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Offline() {
-  return (
-    <div>
-      You are Offline
-    </div>
-  )
-}
