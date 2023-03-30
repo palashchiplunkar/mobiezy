c7185e96b5b5511c17e6bc89115fedebdb27a589
Varshithvhegde <varshithvh@gmail.com>
2023-03-30 16:12:05 +0530

    Formatting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
