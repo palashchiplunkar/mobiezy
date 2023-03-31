c86fdd6f0f73028200208ac66b8a393362869132
Maanikya <maanikya.acharya@gmail.com>
2023-03-31 19:43:58 +0530

    Record Visit Page Partial Completion

--- src/App.js
+++ src/App.js
@@ -23,58 +23,79 @@ import EditSetTopBox from "./views/EditSetTopBox";
 import STBHistory from "./views/STBHistory";
 
 function App() {
-  window.addEventListener("online", handleConnection);
-  window.addEventListener("offline", handleConnection);
+    window.addEventListener("online", handleConnection);
+    window.addEventListener("offline", handleConnection);
 
-  function handleConnection() {
-    if (navigator.onLine) {
-      console.log("Online");
-    } else {
-      console.log("Offline");
-      window.location.reload();
+    function handleConnection() {
+        if (navigator.onLine) {
+            console.log("Online");
+        } else {
+            console.log("Offline");
+            window.location.reload();
+        }
     }
-  }
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<PrivateRoutes />}>
-          <Route path="/select" element={<SelectLanguage />} />
-          <Route path="/home" element={<HomePage />} />
-          {/* <Route path="/customer" element={<Customer />} /> */}
-          <Route path="/customerDrawer" element={<customerDrawer />} />
-          <Route path="/monthlyReport" element={<MonthReport />} />
-          <Route path="/dailyReport" element={<DailyReport />} />
-          <Route path="/more" element={<MoreOptions />} />
-          <Route path="/areaWiseReport" element={<AreaWiseReport />} />
-          <Route
-            path="/subExpiryReport"
-            element={<SubscriptionExpiryReport />}
-          />
-          <Route path="/customerStatistics" element={<CustomerStatistics />} />
-          <Route path="/complaints" element={<Complaints />} />
-          <Route path="/print" element={<Print />} />
-          <Route path="/customer">
-            <Route index element={<Customer />} />
-            <Route path="collectPayment">
-              <Route index element={<CollectPayment />} />
-              <Route path="history">
-                <Route index element={<PaymentHistory />} />
-                <Route path="stbHistory" element={<STBHistory />} />
-              </Route>
-              <Route path="recordVisit">
-                <Route index element={<RecordVisit />} />
-                <Route path="historyVisit" element={<HistoryVisit />} />
-              </Route>
-              <Route path="editCustomer" element={<EditCustomer />} />
-              <Route path="editSetTopBox" element={<EditSetTopBox />} />
-            </Route>
-          </Route>
-        </Route>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<PrivateRoutes />}>
+                    <Route path="/select" element={<SelectLanguage />} />
+                    <Route path="/home" element={<HomePage />} />
+                    {/* <Route path="/customer" element={<Customer />} /> */}
+                    <Route
+                        path="/customerDrawer"
+                        element={<customerDrawer />}
+                    />
+                    <Route path="/monthlyReport" element={<MonthReport />} />
+                    <Route path="/dailyReport" element={<DailyReport />} />
+                    <Route path="/more" element={<MoreOptions />} />
+                    <Route
+                        path="/areaWiseReport"
+                        element={<AreaWiseReport />}
+                    />
+                    <Route
+                        path="/subExpiryReport"
+                        element={<SubscriptionExpiryReport />}
+                    />
+                    <Route
+                        path="/customerStatistics"
+                        element={<CustomerStatistics />}
+                    />
+                    <Route path="/complaints" element={<Complaints />} />
+                    <Route path="/print" element={<Print />} />
+                    <Route path="/customer">
+                        <Route index element={<Customer />} />
+                        <Route path="collectPayment">
+                            <Route index element={<CollectPayment />} />
+                            <Route path="history">
+                                <Route index element={<PaymentHistory />} />
+                                <Route
+                                    path="stbHistory"
+                                    element={<STBHistory />}
+                                />
+                            </Route>
+                            <Route path="recordVisit">
+                                <Route index element={<RecordVisit />} />
+                                <Route
+                                    path="historyVisit"
+                                    element={<HistoryVisit />}
+                                />
+                            </Route>
+                            <Route
+                                path="editCustomer"
+                                element={<EditCustomer />}
+                            />
+                            <Route
+                                path="editSetTopBox"
+                                element={<EditSetTopBox />}
+                            />
+                        </Route>
+                    </Route>
+                </Route>
+                <Route path="/" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
