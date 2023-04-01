6a8e898c78cdc69ddae1e8c2de455ee4e4736a2c
Varshithvhegde <varshithvh@gmail.com>
2023-04-01 12:44:13 +0530

    ALl

--- src/views/CollectPayment.js
+++ src/views/CollectPayment.js
@@ -12,7 +12,6 @@ import "../css/global.css";
 export default function CollectPayment() {
   const navigate = useNavigate();
   const customer = useLocation();
-  
 
   const [stbOpen, setstbOpen] = useState(false);
   const [cusOpen, setcusOpen] = useState(false);
