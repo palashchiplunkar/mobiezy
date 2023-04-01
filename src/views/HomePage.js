b485c0c25d0c6520864cbcf3b6be09aef700c350
prathijna <palashchiplunkar51@gmail.com>
2023-04-01 08:28:14 +0530

    Exit Take 11

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -60,6 +60,46 @@ export default function HomePage() {
         }
     };
 
+    
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+document.addEventListener("backbutton", onBackKeyDown, false);
+function onBackKeyDown() {
+    //Retrieve app's history
+    var history = App.getHistory();
+ 
+    //Check that there's only one screen in history (the current one):
+    if ( history.length === 1 ) {
+        //Check that this element is the default (home) screen:
+        var history_screen = history[0];
+        if ( TemplateTags.getDefaultRouteLink().replace('#','') === history_screen.fragment ) {
+            //Only one element in history and this element is default screen: exit app on back button:
+            navigator.app.exitApp();
+            return;
+        }
+    }
+ 
+    //History has at least one previous element: just go back to it:
+    navigator.app.backHistory();
+}s
+
     useEffect(() => {
         // window.history.pushState({}, "");
         // window.addEventListener("popstate", function (e) {
